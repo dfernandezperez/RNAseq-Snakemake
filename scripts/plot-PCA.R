fbb867822a65fdc13d275030bc2d7fd735978b7c
Fernandez Perez Daniel <ieo4462@hpcfe01.cluster.loc>
2019-04-18 11:49:03 +0200

    Change pca saving options

--- scripts/plot-PCA.R
+++ scripts/plot-PCA.R
@@ -10,7 +10,7 @@ dds <- readRDS(snakemake@input[[1]])
 # obtain normalized counts
 counts <- rlog(dds, blind = FALSE)
 
-pdf(snakemake@output[[1]], width = 7, height = 7.5)
+pdf(snakemake@output[[1]])
 pcaplot(counts, intgroup = snakemake@params[["pca_labels"]], ellipse = FALSE, text_labels = TRUE)
 pcaplot(counts, intgroup = snakemake@params[["pca_labels"]], ellipse = FALSE, text_labels = FALSE)
 dev.off()
