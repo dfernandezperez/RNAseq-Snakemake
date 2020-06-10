6f0767c3d9fc2607cdbcf72ab0a61b8c8dd2f6b0
Fernandez Perez Daniel <ieo4462@ieo.it>
2020-06-10 10:02:16 +0200

    minor fix

--- workflow/scripts/custom_functions.R
+++ workflow/scripts/custom_functions.R
@@ -89,30 +89,104 @@ GSEA_enrichment <- function(df, pathways.gmt) {
 ##-------------------- Plots --------------------##
 VolcanoPlot <- function(df, xlim=NULL, ylim=NULL, main = NULL, labelSize = 7, pval = 0.1, log2FC = 1) {
   require(ggplot2)
-  
-  p <-  ggplot(data = df, aes(x=log2FoldChange, y=-log10(padj), colour=DEG) ) +
-    geom_point(alpha=0.7, size=2) +
-    
-    annotate("text", label = sum(df$DEG == "Upregulated"), color = "red", y = 0, x = xlim[2], 
+  require(ggrastr)
+
+  df <- mutate(df, shape = "circle")
+
+  df <- mutate(df, shape = ifelse(-log10(padj) >  ylim[2], "triangle", shape))
+  df <- mutate(df, padj = ifelse(-log10(padj) >  ylim[2], 10^-ylim[2], padj))
+
+  df <- mutate(df, shape = ifelse(log2FoldChange > xlim[2], "triangle", shape))
+  df <- mutate(df, shape = ifelse(log2FoldChange < -xlim[2], "triangle", shape))
+
+  df <- mutate(df, log2FoldChange = ifelse(log2FoldChange > xlim[2], xlim[2], log2FoldChange))
+  df <- mutate(df, log2FoldChange = ifelse(log2FoldChange < -xlim[2], -xlim[2], log2FoldChange))
+
+
+  p <-  ggplot(data = na.omit(df), aes(x=log2FoldChange, y=-log10(padj), colour=DEG, shape=shape) ) +
+
+    # geom_point_rast(alpha=0.7, size=1.7, raster.height = 5.15, raster.width = 6, raster.dpi = 400) +
+    geom_point(alpha=0.7, size=1.7) +
+
+    annotate("text", label = sum(df$DEG == "Upregulated"), color = "red", y = 0, x = xlim[2],
              vjust="inward",hjust="inward", size = labelSize) +
     annotate("text", label = sum(df$DEG == "Downregulated"), color = "darkgreen", y = 0, x = xlim[1],
              vjust="inward",hjust="inward", size = labelSize) +
-    
-    theme_classic() +
+
+    theme_classic(base_size = 20) +
     theme(legend.title = element_blank()) +
     theme(legend.position = "top") +
-    
-    
+
+
     ggtitle(main) +
     theme(plot.title = element_text(lineheight=.8, face="bold", hjust = .5)) +
-    
-    xlim(xlim) + ylim(ylim) + 
-    
+
+    xlim(xlim) + ylim(ylim) +
+
     geom_hline(yintercept = -log10(pval), linetype = 2) +
     geom_vline(xintercept = c(-log2FC, log2FC), linetype = 2) +
-    
-    xlab("log2 fold change") + ylab("-log10 p-value") +
-    scale_colour_manual(values=c("darkgreen", "gray", "red") )
-  
-  p
+
+    xlab(bquote(~Log[2]~ "fold change")) + ylab(bquote(~-Log[10]~italic(P))) +
+
+    scale_colour_manual(values=c("Downregulated" = "darkgreen", "NS" = "gray", "Upregulated" = "red"),
+                        labels = c("Downregulated" = "Downregulated", "NotDE" = "NS", "Upregulated" = "Upregulated"),
+                        drop = FALSE) + #Force legend to show always
+
+    guides(shape=FALSE) # Remove legend for shapes
+
+  return(p)
 }
+
+
+
+# VolcanoPlot <- function(df, xlim=NULL, ylim=NULL, main = NULL, labelSize = 7, pval = 0.1, log2FC = 1) {
+#   require(ggplot2)
+#   require(ggrastr)
+
+#   df <- mutate(df, shape = "circle")
+#   df <- mutate(df, shape = ifelse(-log10(padj) >  ylim[2], "triangle_up", shape))
+#   df <- mutate(df, padj = ifelse(-log10(padj) >  ylim[2], 10^-ylim[2], padj))
+
+#   df <- mutate(df, shape = ifelse(log2FoldChange > xlim[2], "triangle_right", shape))
+#   df <- mutate(df, shape = ifelse(log2FoldChange < -xlim[2], "triangle_left", shape))
+
+#   df <- mutate(df, log2FoldChange = ifelse(log2FoldChange > xlim[2], xlim[2], log2FoldChange))
+#   df <- mutate(df, log2FoldChange = ifelse(log2FoldChange < -xlim[2], -xlim[2], log2FoldChange))
+
+
+#   p <-  ggplot(data = na.omit(df), aes(x=log2FoldChange, y=-log10(padj), colour=DEG, shape=shape) ) +
+
+#     # geom_point_rast(alpha=0.7, size=1.7, raster.height = 5.15, raster.width = 6, raster.dpi = 400) +
+#     geom_point(alpha=0.7, size=1.7) +
+    
+#     annotate("text", label = sum(df$DEG == "Upregulated"), color = "red", y = 0, x = xlim[2],
+#              vjust="inward",hjust="inward", size = labelSize) +
+#     annotate("text", label = sum(df$DEG == "Downregulated"), color = "darkgreen", y = 0, x = xlim[1],
+#              vjust="inward",hjust="inward", size = labelSize) +
+
+#     theme_classic(base_size = 20) +
+#     theme(legend.title = element_blank()) +
+#     theme(legend.position = "top") +
+
+
+#     ggtitle(main) +
+#     theme(plot.title = element_text(lineheight=.8, face="bold", hjust = .5)) +
+
+#     xlim(xlim) + ylim(ylim) +
+
+#     geom_hline(yintercept = -log10(pval), linetype = 2) +
+#     geom_vline(xintercept = c(-log2FC, log2FC), linetype = 2) +
+
+#     xlab(bquote(~Log[2]~ "fold change")) + ylab(bquote(~-Log[10]~italic(P))) +
+
+#     scale_colour_manual(values=c("Downregulated" = "darkgreen", "NotDE" = "gray", "Upregulated" = "red"),
+#                         labels = c("Downregulated" = "Downregulated", "NotDE" = "NS", "Upregulated" = "Upregulated"),
+#                         drop = FALSE) + #Force legend to show always
+
+#     scale_shape_manual(values=c("triangle_up" = "\u25B2", "triangle_right" = "\u25BA", "triangle_left" = "\u25C4", "circle" = "\u25CF",
+#                                 drop = FALSE)) +
+
+#     guides(shape=FALSE) # Remove legend for shapes
+
+#   return(p)
+# }
